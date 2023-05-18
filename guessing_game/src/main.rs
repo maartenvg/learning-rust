0b719074fe6e0000d80aafebcf04f4b241a3e326
Maarten van Grootel <git@vangrootel.org>
2023-05-18 12:02:43 -0400

    Loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,25 +4,27 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-
-    println!("Please input your guess.");
-
+    
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    let mut guess = String::new();
-    
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+    loop {
+        println!("Please input your guess.");
 
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
 
-    println!("You guessed: {guess}");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You guessed right!"),
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You guessed right!"),
+        }
     }
 }
